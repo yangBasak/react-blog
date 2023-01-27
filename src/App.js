c431dee5c4d1e41b5e0669aef1fb8eb90bf3549a
yangBasak <yangbasak@gamil.com>
2023-01-27 22:07:14 +0900

    포스트 제목 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState('')
+  
   return (
-    <div>
-      <button>sss</button>
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">제목</label>
+        <input className="form-control" value={title} onChange={(e)=>{
+          setTitle(e.target.value)
+        }} />
+      </div>
+      <button className="btn btn-primary">
+        등록하기
+      </button>
     </div>
   );
 }
